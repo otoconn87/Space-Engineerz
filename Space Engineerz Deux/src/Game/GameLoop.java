de9de66fff7e8c2e39df1ddcea7ffca688255b14
Matthew Clements <matthew.clements@mun.ca>
2014-03-25 10:56:08 -0230

    cleaner game loop

--- Space Engineerz Deux/src/Game/GameLoop.java
+++ Space Engineerz Deux/src/Game/GameLoop.java
@@ -3,7 +3,6 @@ package Game;
 import java.applet.Applet;
 import java.awt.Graphics;
 import java.awt.Image;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
@@ -16,18 +15,18 @@ import Sprites.Lobster;
 import Sprites.Player;
 
 @SuppressWarnings("serial")
-public class GameLoop extends Applet implements Runnable, KeyListener{
+public class GameLoop extends Applet implements Runnable, KeyListener {
 
 	public int x, y;
 	public int walkTimer = 0;
-	 
+
 	public Image offscreen;
 	public Graphics d;
-	
-	public boolean left, right, jump, down; //directional buttons
-	
-	public boolean walking, idling; //character states
-	
+
+	public boolean left, right, jump, down; // directional buttons
+
+	public boolean walking, idling; // character states
+
 	public BufferedImage background, playerAnimations, lobsterAnimations;
 	public LevelOne levelOne;
 	public BufferedImage gameMapBlocked[][];
@@ -35,130 +34,127 @@ public class GameLoop extends Applet implements Runnable, KeyListener{
 	public int[][] levelOneMap;
 	public Player player;
 	public Lobster lobster;
-	
-	
-	public void run(){
-		
-		
+
+	public void run() {
+
 		player = new Player("space_player.png");
 		player.setFacingRight(true);
-		player.setPosition(100,100);
-		
+		player.setPosition(100, 100);
+
 		lobster = new Lobster("space_lobster.png");
 		lobster.setFacingRight(false);
 		lobster.setPosition(400, 100);
-		
-		levelOne = new LevelOne ("level1_space.png", getClass().getResourceAsStream("space_map.map"));
-		//levelOne.loadMap();
+
+		levelOne = new LevelOne("level1_space.png", getClass()
+				.getResourceAsStream("space_map.map"));
+		// levelOne.loadMap();
 		gameMapBlocked = levelOne.getLevelOneBlockedTiles();
 		gameMapPassed = levelOne.getLevelOnePassTiles();
 		levelOneMap = levelOne.getLevelOneMap();
-		
+
 		try {
-			
-			background = ImageIO.read(getClass().getResourceAsStream("citybg.jpg"));
+
+			background = ImageIO.read(getClass().getResourceAsStream(
+					"citybg.jpg"));
 
 		} catch (IOException e1) {
-			
+
 			e1.printStackTrace();
 		}
-		
-		while(true){
-			
-			if(left == true){
 
-				player.setLeft();
-			}
-			else if(right == true){
+		while (true) {
 
-				player.setRight();
-			}
-			else if(jump == true){
-
-				
-				player.setJump();
-			}
-			else if(down == true){
-				if(left == true )player.setFacingRight(false);
-				
-			}
-			else{
-				player.setIdling();
-			}
-			
+			playerMovement();
 			lobsterMovement();
 			checkIntersection();
-			
+
 			repaint();
 			try {
 				Thread.sleep(15);
 			} catch (InterruptedException e) {
-					e.printStackTrace();
+				e.printStackTrace();
 			}
 		}
 	}
-	
+
+	private void playerMovement() {
+		if (left == true) {
+
+			player.setLeft();
+		} else if (right == true) {
+
+			player.setRight();
+		} else if (jump == true) {
+
+			player.setJump();
+		} else if (down == true) {
+			if (left == true)
+				player.setFacingRight(false);
+
+		} else {
+			player.setIdling();
+		}
+	}
+
+	private void lobsterMovement() {
+
+		if (!lobster.facingRight) {
+			lobster.setLeft();
+		} else {
+			lobster.setRight();
+		}
+
+		if (lobster.getX() == 200) {
+			lobster.setRight();
+		}
+		if (lobster.getX() == 400) {
+			lobster.setLeft();
+		}
+
+	}
+
 	private void checkIntersection() {
-				
-		if(lobster.getRect().intersects(player.getRect())){
+
+		if (lobster.getRect().intersects(player.getRect())) {
 			System.out.println("Intersection");
 		}
 	}
 
-	private void lobsterMovement(){
-		
-		if(!lobster.facingRight){
-			lobster.setLeft();
-		}else{
-			lobster.setRight();
-		}
-		
-		if(lobster.getX() == 200){
-			lobster.setRight();
-		}
-		if(lobster.getX() == 400){
-			lobster.setLeft();
-		}
-		
-	}
-
-
 	public void keyPressed(KeyEvent key) {
-		
-		if(key.getKeyCode() == 37){
+
+		if (key.getKeyCode() == 37) {
 			left = true;
 		}
-		if(key.getKeyCode() == 39){
+		if (key.getKeyCode() == 39) {
 			right = true;
 		}
-		if(key.getKeyCode() == 87){
+		if (key.getKeyCode() == 87) {
 			jump = true;
 		}
-		if(key.getKeyCode() == 40){
+		if (key.getKeyCode() == 40) {
 			down = true;
 		}
 	}
 
-	
 	public void keyReleased(KeyEvent key) {
-		if(key.getKeyCode() == 37){
+		if (key.getKeyCode() == 37) {
 			left = false;
 		}
-		if(key.getKeyCode() == 39){
+		if (key.getKeyCode() == 39) {
 			right = false;
 		}
-		if(key.getKeyCode() == 87){
+		if (key.getKeyCode() == 87) {
 			jump = false;
 		}
-		if(key.getKeyCode() == 40){
+		if (key.getKeyCode() == 40) {
 			down = false;
 		}
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent key) {
 		// unused
-		
+
 	}
 }
